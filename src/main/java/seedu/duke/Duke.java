e8a0f2227c557759976db2b82ee792100b4f4405
thaddyyz <76462596+thaddyyz@users.noreply.github.com>
2021-09-30 15:45:55 +0800

    added menu

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -7,15 +7,24 @@ public class Duke {
      * Main entry-point for the java.duke.Duke application.
      */
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
+
+        String logo = " ____        _        \n" + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n" + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("What is your name?");
 
         Scanner in = new Scanner(System.in);
         System.out.println("Hello " + in.nextLine());
+        System.out.println("Welcome to LOTS");
+        String input;
+        Menu menu = new Menu();
+        int runcode = 0;
+        while (runcode == 0) {
+            input = in.nextLine();
+            if (input.indexOf("menu") != -1) {
+                menu.printmenu();
+            }
+        }
     }
 }
