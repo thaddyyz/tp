606abf1c7411e8f5884f25af25994c8bcbfbc7ee
Wai Kit Lau <waikit.lau98@gmail.com>
2021-10-07 21:54:24 +0800

    Add AddCommand Class and Add static keyword to listOfPeople.

---
+++ src/main/java/seedu/duke/AddCommand.java
@@ -0,0 +1,84 @@
+package seedu.duke;
+
+public class AddCommand extends Command {
+
+    public static final String COMMAND_WORD = "add";
+    private final String personName;
+    private final Integer foodIndex;
+    private final Integer foodQuantity;
+
+    public AddCommand(String input) {
+        Integer[] indexOfSlashes = indexOfSlashes(input);
+        this.personName = getPersonName(input, indexOfSlashes[0], indexOfSlashes[1]);
+        this.foodIndex = getFoodIndex(input, indexOfSlashes[1], indexOfSlashes[2]);
+        this.foodQuantity = getFoodQuantity(input, indexOfSlashes[2]);
+    }
+
+    /**
+     * Adds a new person with the given attributes into the list of people.
+     */
+    @Override
+    public void execute() {
+        Person person = new Person(personName);
+        person.addFoodToIndividualFoodOrders(foodIndex);
+        PeopleManager.listOfPeople.add(person);
+    }
+
+    /**
+     * Obtain index of "/" in the string to be use to split input strings into substrings.
+     * @param input User input.
+     * @return an array of index of "/".
+     */
+    private static Integer[] indexOfSlashes(String input) {
+
+        Integer[] indexOfSlashes = new Integer[3];
+
+        //Index firstSlashIndex to secondSlashIndex holds Person Name.
+        int firstSlashIndex = input.indexOf("/");
+        //Index secondSlashIndex to thirdSlashIndex holds index of Food.
+        int secondSlashIndex = input.indexOf("/", firstSlashIndex + 1);
+        //Index thirdSlashIndex to last index holds food quantity.
+        int thirdSlashIndex = input.indexOf("/", secondSlashIndex + 1);
+
+        indexOfSlashes[0] = firstSlashIndex;
+        indexOfSlashes[1] = secondSlashIndex;
+        indexOfSlashes[2] = thirdSlashIndex;
+
+        return indexOfSlashes;
+    }
+
+    /**
+     * Function used to parse the user input to get person name.
+     * @param input User input.
+     * @param indexOfFirstSlash First "/" index.
+     * @param indexOfSecondSlash Second "/" index.
+     * @return Person Name in String type.
+     */
+    private static String getPersonName(String input, int indexOfFirstSlash, int indexOfSecondSlash) {
+        String tempPersonName = input.substring(indexOfFirstSlash + 1,indexOfSecondSlash - 1);
+        return tempPersonName;
+    }
+
+    /**
+     * Function used to parse the user input to get food index.
+     * @param input User input.
+     * @param indexOfSecondSlash Second "/" index.
+     * @param indexOfThirdSlash Third "/" index.
+     * @return Food index in Integer type.
+     */
+    private static Integer getFoodIndex(String input, int indexOfSecondSlash, int indexOfThirdSlash) {
+        int foodIndex = Integer.parseInt(input.substring(indexOfSecondSlash + 1, indexOfThirdSlash - 1));
+        return foodIndex;
+    }
+
+    /**
+     * Function used to parse the user input to get food index.
+     * @param input User input.
+     * @param indexOfThirdSlash Third "/" index.
+     * @return Food Quantity in Integer type.
+     */
+    private static Integer getFoodQuantity(String input, int indexOfThirdSlash) {
+        int foodQuantity = Integer.parseInt(input.substring(indexOfThirdSlash + 1, input.length()));
+        return foodQuantity;
+    }
+}
