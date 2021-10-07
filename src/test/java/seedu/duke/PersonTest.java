63cebf01a3b04459644c62f9404a1e4aa71c6ac3
WaiKit-nus <waikit.lau98@gmail.com>
2021-10-07 15:42:01 +0800

    Update PersonTest JUnit.

--- src/test/java/seedu/duke/PersonTest.java
+++ src/test/java/seedu/duke/PersonTest.java
@@ -9,48 +9,35 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 public class PersonTest {
 
     /**
-     * Test case to check if the individualFoordOrders Array is populated with
-     * int 0 at the start.
-     * @throws Exception thrown when the array is not populated with int 0.
+     * Checks if the specified index value will pass.
+     * after being inputted into the addFoodToIndividualFoodOrders function.
+     * @throws Exception when the output is incorrect.
      */
     @Test
-    public void setupIndividualFoodOrders_emptyArray() throws Exception {
-        for (int i = 0; i < Menu.TOTAL_MENU_ITEMS; i++) {
-            assertEquals(0, initialisePersonClass().individualFoodOrders[i]);
-        }
-    }
-
-    /**
-     * Checks if the personName is null or not.
-     * @throws Exception if the personName variable is Null.
-     */
-    @Test
-    public void personName_notNull() throws Exception {
-        assertNotNull(initialisePersonClass().personName);
-    }
-
-    /**
-     * Checks if the increment is 1 when using the addFoodToIndividualFoodOrders function.
-     * Checks if the increment is at the right index.
-     * @throws Exception when the increment is not increased by 1 and is at the wrong index.
-     */
-    @Test
-    public void addFoodToIndividualFoodOrders_increment() throws Exception {
+    public void addFoodToIndividualFoodOrders_incrementIndex2ValueBy1_Index2ValueIncreasedBy1() throws Exception {
         Person trialPerson = initialisePersonClass();
-        trialPerson.addFoodToIndividualFoodOrders(2);
-        assertEquals(1,trialPerson.individualFoodOrders[2]);
+        //FoodIndex = -2 is not within the array.
+        assertEquals("Please enter the right menu number!",trialPerson.addFoodToIndividualFoodOrders(-2));
+        //FoodIndex = 11 is not within the array.
+        assertEquals("Please enter the right menu number!", trialPerson.addFoodToIndividualFoodOrders(11));
+        //FoodIndex = 2 is within the array. Function should enter if() loop.
+        assertEquals("Completed!",trialPerson.addFoodToIndividualFoodOrders(2));
     }
 
     /**
-     * Checks if the specified index value will remain 0
-     * after using the removeFoodFromIndividualFoodOrders function.
-     * @throws Exception if the value is not 0.
+     * Checks if the specified index value will pass.
+     * after being inputted into the removeFoodFromIndividualFoodOrders function.
+     * @throws Exception if the value is not the same.
      */
     @Test
     public void removeFoodFromIndividualFoodOrders_remainsZero() throws Exception {
         Person trialPerson = initialisePersonClass();
-        trialPerson.removeFoodFromIndividualFoodOrders(2);
-        assertEquals(0, trialPerson.individualFoodOrders[2]);
+        //FoodIndex = -3 is not within the array.
+        assertEquals("Please enter the right menu number!", trialPerson.removeFoodFromIndividualFoodOrders(-3));
+        //FoodIndex = 30 is not within the array.
+        assertEquals("Please enter the right menu number!", trialPerson.removeFoodFromIndividualFoodOrders(30));
+        //FoodIndex = 3 is within the array.
+        assertEquals("Completed!", trialPerson.removeFoodFromIndividualFoodOrders(3));
     }
 
     /**
