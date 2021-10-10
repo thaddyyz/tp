36eb5dfc76300f54c64a4c7cb639f58b0bba9521
thaddyyz <76462596+thaddyyz@users.noreply.github.com>
2021-10-10 11:56:44 +0800

    minor ui edit

--- src/main/java/seedu/duke/Ui.java
+++ src/main/java/seedu/duke/Ui.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Ui {
 
-    static final String line = "--------------------------------------------------";
+    private static final String line = "--------------------------------------------------";
     private static final String BORDER = "=================================="
             + "============================="
             + "=============================";
