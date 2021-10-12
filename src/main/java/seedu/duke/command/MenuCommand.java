4d369434791de1f5bcdc25f6e1d915d227426aaa
andrewtkh1 <andrewtkh1@gmail.com>
2021-10-12 19:18:29 +0800

    Add more output-redirect test & removed function to read from file.

--- src/main/java/seedu/duke/command/MenuCommand.java
+++ src/main/java/seedu/duke/command/MenuCommand.java
@@ -5,8 +5,6 @@ import seedu.duke.Menu;
 import seedu.duke.Ui;
 
 import java.io.FileNotFoundException;
-import java.util.Scanner;
-import java.io.File;
 import java.util.ArrayList;
 
 public class MenuCommand extends Command {
@@ -44,25 +42,7 @@ public class MenuCommand extends Command {
      * Get menu data from foodData.txt
      */
     private void getMenu() throws FileNotFoundException {
-        File myObject = new File("data/foodData.txt");
-        Scanner myReader = new Scanner(myObject);
-        extractMenuData(myReader);
-        myReader.close();
-    }
-
-    /**
-     * extractMenuData seperates txt file content into individual lines
-     * and further seperates each line into food name and price to be
-     * stored in seperate arraylists.
-     *
-     * @param myReader contains menu data collected from the txt file.
-     */
-    private void extractMenuData(Scanner myReader) {
-        while (myReader.hasNextLine()) {
-            String data = myReader.nextLine();
-            foodList.add(data.substring(0, data.indexOf("|")));
-            priceList.add(Double.parseDouble(data.substring(data.indexOf("|") + 1)));
-        }
+        extractLocalMenuData();
     }
 
     private void extractLocalMenuData() {
