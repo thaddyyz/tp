6161f98d7dd36bfacdefa97be047a2ed388cb3c3
markuslyq <markuslyq@gmail.com>
2021-10-12 14:12:03 +0800

    Update DeleteCommandTest.java

--- src/test/java/seedu/duke/DeleteCommandTest.java
+++ src/test/java/seedu/duke/DeleteCommandTest.java
@@ -3,12 +3,36 @@ package seedu.duke;
 import org.junit.jupiter.api.Test;
 
 import java.util.LinkedList;
+import seedu.duke.AddCommand;
+import seedu.duke.Command;
+import seedu.duke.DeleteCommand;
+import seedu.duke.LotsException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DeleteCommandTest {
 
     protected PeopleManager emptyPeopleManager;
+    protected PeopleManager peopleManagerWithThreeOrders;
+
+    void setUp() throws LotsException {
+        String input1 = "add /n markus /i 1 /q 1";
+        Command command1 = new AddCommand(input1);
+        command1.setData(peopleManagerWithThreeOrders);
+        command1.execute();
+
+        String input2 = "add /n adam /i 4 /q 1";
+        Command command2 = new AddCommand(input2);
+        command2.setData(peopleManagerWithThreeOrders);
+        command2.execute();
+
+        String input3 = "add /n andrew /i 8 /q 1";
+        Command command3 = new AddCommand(input3);
+        command3.setData(peopleManagerWithThreeOrders);
+        command3.execute();
+
+    }
+
 
     @Test
     void execute_emptyOrderList_expectException() throws LotsException {
@@ -18,4 +42,6 @@ class DeleteCommandTest {
         assertThrows(NullPointerException.class,
             () -> command.execute());
     }
+
+    void execute_inputWithoutFoodIndex_expect
 }
