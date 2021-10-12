b928f56f2515cec2263ff2cf731988a4c3218b66
mohamad-adam8991 <69377976+mohamad-adam8991@users.noreply.github.com>
2021-10-12 20:13:25 +0800

    Added JUnit test for orders command

--- src/test/java/seedu/duke/UiTest.java
+++ src/test/java/seedu/duke/UiTest.java
@@ -1,7 +1,20 @@
 package seedu.duke;
 
 import org.junit.jupiter.api.Test;
+import seedu.duke.command.Command;
+import seedu.duke.exceptions.LotsException;
 
 public class UiTest {
+
+    @Test
+    void execute_inputContainsExtraInputs_throwsLotsException() throws LotsException {
+        try {
+            Command command;
+            command = Parser.getCommand("list a");
+            command.execute();
+        } catch (LotsException e) {
+            System.out.println("Please make sure there is no inputs after the list command!");
+        }
+    }
 
 }
