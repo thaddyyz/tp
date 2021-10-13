9625837bd3c7301525290c430d8cc252f4b4f470
thaddyyz <76462596+thaddyyz@users.noreply.github.com>
2021-10-13 13:24:57 +0800

    update PeopleManagerTest and PersonTest

---
+++ src/test/java/seedu/duke/PeopleManagerTest.java
@@ -0,0 +1,50 @@
+package seedu.duke;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import seedu.duke.exceptions.LotsException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;;
+
+public class PeopleManagerTest {
+    protected PeopleManager peopleManager = new PeopleManager();
+    
+
+    @BeforeEach
+    void setUp() throws LotsException {
+        String[] names = {"Markus", "Adam", "Andrew"};
+        for (int i = 0; i < 3; i++) {
+            Person person = new Person(names[i]);
+            peopleManager.addPerson(person);
+        }
+    }
+
+    @Test
+    void countPeopleInList_Test() {
+        assertEquals(3, peopleManager.countPeopleInList());
+    }
+
+    @Test
+    void getPerson_Test() {
+        assertEquals("Adam", peopleManager.getPerson(1));
+    }
+
+    @Test
+    void getSize_Test() {
+        assertEquals(3, peopleManager.getSize());
+    }
+
+    @Test
+    void isEmpty_Test() {
+        assertEquals(false, peopleManager.isEmpty());
+    }
+
+    @Test
+    public void deletePerson_InvalidPersonIndex() {
+        assertThrows(LotsException.class, () -> {
+            peopleManager.deletePerson(5);
+        } );
+    }
+
+}
--- src/test/java/seedu/duke/PersonTest.java
+++ src/test/java/seedu/duke/PersonTest.java
@@ -1,11 +1,57 @@
 package seedu.duke;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import seedu.duke.exceptions.LotsException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;;
 
 public class PersonTest {
+    private String TEST_NAME = "hello";
 
+    @Test
+    public void addFoodToIndividualFoodOrders_Negative_Zero() {
+        Person person = new Person(TEST_NAME);
+        assertThrows(LotsException.class, () -> {
+            person.addFoodToIndividualFoodOrders(-1);
+        } );
+        assertThrows(LotsException.class, () -> {
+            person.addFoodToIndividualFoodOrders(0);
+        } );
+    }
+
+    @Test
+    public void addFoodToIndividualFoodOrders_MoreThantotalMenuItems() {
+        Person person = new Person(TEST_NAME);
+        assertThrows(LotsException.class, () -> {
+            person.addFoodToIndividualFoodOrders(88);
+        } );
+    }
+    
+    @Test
+    public void removeFoodFromIndividualFoodOrders_Negative_Zero() {
+        Person person = new Person(TEST_NAME);
+        assertThrows(LotsException.class, () -> {
+            person.removeFoodFromIndividualFoodOrders(-1,1);
+        } );
+        assertThrows(LotsException.class, () -> {
+            person.removeFoodFromIndividualFoodOrders(0,1);
+        } );
+    }
+
+    @Test
+    public void removeFoodFromIndividualFoodOrders_MoreThantotalMenuItems() {
+        Person person = new Person(TEST_NAME);
+        assertThrows(LotsException.class, () -> {
+            person.removeFoodFromIndividualFoodOrders(88,1);
+        } );
+    }
+
+    /*@Test
+    public void deleteParticularOrder_OutOfRange() {
+        Person person = new Person(TEST_NAME);
+        assertThrows(LotsException.class, () -> {
+            person.deleteParticularOrder(person.numberOfOrders+1);
+        } );
+    }*/
+ 
 }
